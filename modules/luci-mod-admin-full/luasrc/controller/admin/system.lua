9724f1b64e97faad294e117b25e23f06b27b2023
feng649222057 <wind649@vip.qq.com>
2023-02-25 22:55:42 +0800

    优化调整

--- modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
+++ modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
@@ -14,18 +14,19 @@ function index()
 	entry({"admin", "system", "admin"}, cbi("admin_system/admin"), _("Administration"), 2)
 
 	if fs.access("/bin/opkg") then
-		entry({"admin", "system", "packages"}, post_on({ exec = "1" }, "action_packages"), _("Software"), 10)
+		entry({"admin", "system", "packages"}, post_on({ exec = "1" }, "action_packages"), _("Software"), 3)
 		entry({"admin", "system", "packages", "ipkg"}, form("admin_system/ipkg"))
 	end
 
-	entry({"admin", "system", "startup"}, form("admin_system/startup"), _("Startup"), 45)
-	entry({"admin", "system", "crontab"}, form("admin_system/crontab"), _("Scheduled Tasks"), 46)
+	entry({"admin", "system", "startup"}, form("admin_system/startup"), _("Startup"), 4)
 
 	if fs.access("/sbin/block") and fs.access("/etc/config/fstab") then
-		entry({"admin", "system", "fstab"}, cbi("admin_system/fstab"), _("Mount Points"), 50)
+		entry({"admin", "system", "fstab"}, cbi("admin_system/fstab"), _("Mount Points"), 5)
 		entry({"admin", "system", "fstab", "mount"}, cbi("admin_system/fstab/mount"), nil).leaf = true
 		entry({"admin", "system", "fstab", "swap"},  cbi("admin_system/fstab/swap"),  nil).leaf = true
 	end
+
+	entry({"admin", "system", "crontab"}, form("admin_system/crontab"), _("Scheduled Tasks"), 46)
 
 	local nodes, number = nixio.fs.glob("/sys/class/leds/*")
 	if number > 0 then
