1e5d0d87bc5bf451026ef222ce68bf6ba9dea8ad
LiXiang <li_xiang@hisuntech.com>
2023-02-28 19:56:08 +0800

    添加软件包

--- applications/luci-app-wol/luasrc/controller/wol.lua
+++ applications/luci-app-wol/luasrc/controller/wol.lua
@@ -1,6 +1,7 @@
 module("luci.controller.wol", package.seeall)
 
 function index()
-	entry({"admin", "services", "wol"}, form("wol"), _("Wake on LAN"), 90)
-	entry({"mini", "services", "wol"}, form("wol"), _("Wake on LAN"), 90)
+    entry({ "admin", "control" }, firstchild(), "Control", 44).dependent = false
+    entry({ "admin", "control", "wol" }, form("wol"), _("Wake on LAN"), 90)
+    entry({ "mini", "control", "wol" }, form("wol"), _("Wake on LAN"), 90)
 end
